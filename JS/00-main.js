13335e19e4cec9d555db9f012c72688ef3f261e0
ssinare <82803125+ssinare@users.noreply.github.com>
2021-05-03 11:30:36 +0300

    Intro

---
+++ JS/00-main.js
@@ -0,0 +1,19 @@
+// vienos eilut4s komentaras
+
+/*
+keliu
+eiluciu
+komentaras
+*/
+
+/*
+
+MOKYMOSI TEMOS:
+
+1. kintamieji - atsiminti darbine informacija
+2. palyginimo salygos
+3. ciklas - ejimas per saraso narius
+4. junkcijos - pasikartojancios logikos perpanaudojimas
+5. ivykiai - mouse click, keyboard click, scroll, upload end, page load....
+
+*/
