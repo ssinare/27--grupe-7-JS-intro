36f73bbf796b9a9a731412926e9af84dd3122029
ssinare <82803125+ssinare@users.noreply.github.com>
2021-05-03 12:32:10 +0300

    Variables

---
+++ JS/01-variables.js
@@ -0,0 +1,51 @@
+/*
+KINTAMIEJI:
+
+tipai, pagal iniciavimo buda:
+- const (default)
+- let
+- var (nenaudoti)
+
+
+pagal informacijos tipa:
+- number (skaicius)
+- string (tekstas)
+- boolean (logine reiksme)
+- array (sarasas)
+- null
+- undefined
+
+*/
+
+const vardas = 'Vardenis';
+const amzius = 99;
+const buvomDuse = true;
+const dabarYraNaktis = false;
+const pazymiai = [5, 2, 8, 10];
+const draugai = ['Rika','Nedas', 'Ieva'];
+const maryte = {
+    vardas: 'Maryte',
+    amzius: 87,
+    megstamaSpalva: 'green',
+    baigtaKlasiu: 12,
+    gyvvenamojiVieta: {
+        miestas: 'Zagare',
+        gatve: 'tyrelio',
+        namoNumeris: 8
+    }
+}
+
+const bendraPazymiuSuma = 0;
+console.log(amzius);
+console.log(bendraPazymiuSuma);
+console.log(bendraPazymiuSuma);
+console.log(bendraPazymiuSuma);
+console.log(bendraPazymiuSuma);
+
+
+const rankos = 2;
+console.log(rankos);
+
+const kojos = 5;
+
+const draugai = 6;
