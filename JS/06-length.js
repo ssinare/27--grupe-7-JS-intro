01adfdc0ae2122ac26c495ed7c99adf875d9734b
ssinare <82803125+ssinare@users.noreply.github.com>
2021-05-04 22:58:03 +0300

    Ilgiai

---
+++ JS/06-length.js
@@ -0,0 +1,30 @@
+const hi = 'Labas rytas.';
+const hiSize = hi.length;
+
+// Tekto "Labas" ilgis yra 5 simboliai.
+const ans = `Tekto "${hi}" ilgis yra ${hiSize} simboliai.`;
+
+console.log(ans);
+
+
+const marks = [10, 2, 8, 4, 6];
+const marksCount = marks.length;
+
+// Studentas turi 4 pazymius.
+const student = `Studentas turi ${marksCount} pazymius.`;
+
+console.log(student);
+
+const priesai = ['Petras', 'Jonas', 'Antanas', 'Stasys'];
+const priesaiSize = priesai.length;
+console.log(priesaiSize);
+const VarduIlgiai = `Priesu vardu "${priesai[2]}" vardo ilgis yra ${priesaiSize[0]} simboliai`; 
+console.log(VarduIlgiai);
+
+
+
+const miega = 'Sunys miega po stalu.'
+const miegaSize = miega.length;
+// Sakinio Sunys miega po stalu ilgis yra:
+const sakinioIlgis = `Sakinio "${miega}" simboliu skaicius yra ${miegaSize}.`;
+console.log(sakinioIlgis);
