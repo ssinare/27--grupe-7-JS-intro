605e0ecb9a9e8fd70223cb72019b90e8fc66fed4
ssinare <82803125+ssinare@users.noreply.github.com>
2021-05-04 21:56:53 +0300

    ternary

---
+++ JS/05-ternary.js
@@ -0,0 +1,22 @@
+/*
+TERNARY - sutrumpintas if'as
+jam yra butinos "true" ir "false" dalys
+struktura:
+klausimas ? jei tiesa : jei ne tiesa
+*/
+
+const amzius1 = 14;
+const cenzas = 18;
+
+const arIleisti = amzius1 >= cenzas ? true :false;
+console.log('Ar ileisti:', arIleisti);
+
+const arVyresnis = amzius1 >= cenzas ? 'vyresnis' : 'neVyresnis';
+console.log('Ar  vyresnis:', arVyresnis);
+
+
+const vartai = 'uzdaryti';
+
+const suoPabego = vartai === 'atidaryti' ? 'suo pabego':'suo sedi kieme';
+console.log('Kur suo? ', suoPabego);
+
